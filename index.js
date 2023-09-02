f8860fa4c1548f647562366f0d67868f84d3495f
Md <shoyebmd424@gmail.com>
2023-09-02 22:47:43 +0530

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import AuthRouter from "./Router/Auth.js";
 import StudentRouter from "./Router/Student.js";
 import cors from "cors";
 import path from "path";
-dotenv.config({ path: "./config.env" });
+// dotenv.config({ path: "./config.env" });
 dotenv.config();
 const __dirname = path.resolve();
 const app = Express();
@@ -16,12 +16,13 @@ const PORT = process.env.PORT || 8080;
 app.use(Express.json());
 app.use(cors());
 app.use(Express.static(path.join(__dirname, "./frontEnd/build")));
+
 app.use("/api/auth", AuthRouter);
 app.use("/api/student", StudentRouter);
 
 //rest api
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./frontEnd/build/index.html"));
 });
 app.listen(PORT, () => {
   Db;
